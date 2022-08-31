70c182c852d736d5ef1bab7905c24836c2f0f575
Shubham Shah <65122647+shubhxms@users.noreply.github.com>
2022-09-01 00:35:14 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '426aed2971104908a1f07d4cdab61d3b',
+  rootNotionPageId: '60f0eb5c08b147078e7318397419c35a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Build websites with Notion',
-  domain: 'jotzilla.vercel.app',
-  author: 'Jotzilla',
+  name: 'shubham',
+  domain: 'shubhxms.github.io',
+  author: 'shubham',
 
   // open graph metadata (optional)
   description: 'Learn how to build websites with Notion. This website is an example starter site powered by Notion. Built on top of NotionX.',
